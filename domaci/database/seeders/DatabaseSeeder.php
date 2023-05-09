b4e5cb5b0ee0112d0020c781ff72955c93bfb344
Jovana Mitic <mitic.jovana00@gmail.com>
2023-05-09 14:27:18 +0200

    Dodate migracije, NastavnikFactory i definisan seeder pomoću kog smo ubacili podatke u tabele

--- domaci/database/seeders/DatabaseSeeder.php
+++ domaci/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\User;
+use \App\Models\Jezik;
+use \App\Models\Nastavnik;
+use \App\Models\Ocena;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        //brisanje svega iz tabela
+        User::truncate();
+        Nastavnik::truncate();
+        Jezik::truncate();
+        Ocena::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(5)->create();
+        Nastavnik::factory(10)->create();
+
+        $jezik1 = Jezik::create([
+            'naziv' => 'španski'
+        ]);
+
+        $jezik2 = Jezik::create([
+            'naziv' => 'engleski'
+        ]);
+
+        $jezik3 = Jezik::create([
+            'naziv' => 'italijanski'
+        ]);
+
+        $jezik4 = Jezik::create([
+            'naziv' => 'japanski'
+        ]);
+
+        $jezik5 = Jezik::create([
+            'naziv' => 'francuski'
+        ]);
+
+        $ocena1 = Ocena::create([
+            'datumIVreme' => now(),
+            'user' => 1,
+            'jezik' => 1,
+            'nastavnik'=>2,
+            'note' => 'Validno'
+        ]);
+
+        $ocena2 = Ocena::create([
+            'datumIVreme' => now(),
+            'user' => 2,
+            'jezik' => 1,
+            'nastavnik'=>1,
+            'note' => 'Validno'
+        ]);
     }
 }
